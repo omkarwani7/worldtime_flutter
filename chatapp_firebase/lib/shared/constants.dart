b2e391b2c208d291b17fd9491f02d05c562c99a1
omkarwani7 <130388795+omkarwani7@users.noreply.github.com>
2023-04-16 20:28:21 +0530

    Delete chatapp_firebase directory

--- chatapp_firebase/lib/shared/constants.dart
+++
@@ -1,6 +0,0 @@
-class Constants {
-  static String appId = "1:139527284166:web:f8747f55295f3565fd3ec7";
-  static String apiKey = "AIzaSyDceKoRZUT0vzE9l6jy8VZyDjD0AvNvkQE";
-  static String messagingSenderId = "139527284166";
-  static String projectId = "chatappflutter-8c1d2";
-}
